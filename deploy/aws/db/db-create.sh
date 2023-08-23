c0194b7c9b6044d60b7481449c6b5e52565c30a9
Peter Kneale <pkneale@cmri.org.au>
2023-08-23 12:33:14 +1000

    Include database schema creation and data population

--- deploy/aws/db/db-create.sh
+++ deploy/aws/db/db-create.sh
@@ -18,7 +18,7 @@ APP="splicevault"
 
 CF_STACK_NAME="au-${ENV}-${APP}-db"
 CF_INSTANCE_NAME="au_${ENV}_${APP}_database"
-DB_NAME="au_${ENV}_${APP}"
+DB_DATABASE="au_${ENV}_${APP}"
 DB_USERNAME="au_${ENV}_${APP}"
 DB_PASSWORD="au_${ENV}_${APP}"
 
@@ -31,22 +31,63 @@ echo "DB_HOST = $DB_HOST"
 echo "DB_MASTER_DATABASE = $DB_MASTER_DATABASE"
 echo "DB_MASTER_USERNAME = $DB_MASTER_USERNAME"
 echo "DB_MASTER_PASSWORD = $DB_MASTER_PASSWORD"
-echo "DB_NAME = $DB_NAME"
+echo "DB_DATABASE = $DB_DATABASE"
 echo "DB_USERNAME = $DB_USERNAME"
 echo "DB_PASSWORD = $DB_PASSWORD"
 
-echo "Creating database ${DB_NAME} and user ${DB_USERNAME}..."
-echo "CREATE DATABASE ${DB_NAME};CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';" | docker run -i --rm \
+echo "Creating database ${DB_DATABASE} and user ${DB_USERNAME}..."
+echo "CREATE DATABASE ${DB_DATABASE};CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';" | docker run -i --rm \
   -e PGHOST=$DB_HOST \
   -e PGDATABASE=$DB_MASTER_DATABASE \
   -e PGUSER=$DB_MASTER_USERNAME \
   -e PGPASSWORD=$DB_MASTER_PASSWORD \
   postgres psql
 
-echo "Granting privileges on database ${DB_NAME} to user ${DB_USERNAME}..."
-echo "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} to ${DB_USERNAME};" | docker run -i --rm \
+echo "Granting privileges on database ${DB_DATABASE} to user ${DB_USERNAME}..."
+echo "GRANT ALL PRIVILEGES ON DATABASE ${DB_DATABASE} to ${DB_USERNAME};" | docker run -i --rm \
   -e PGHOST=$DB_HOST \
-  -e PGDATABASE=$DB_NAME \
+  -e PGDATABASE=$DB_DATABASE \
   -e PGUSER=$DB_MASTER_USERNAME \
   -e PGPASSWORD=$DB_MASTER_PASSWORD \
   postgres psql
+
+FILES=( "ref_exons" ) 
+
+echo "Configured with host $DB_HOST database $DB_MASTER_USERNAME and user $DB_MASTER_PASSWORD"
+
+echo "Dropping database schema"
+cat ./db-down.sql | docker run --rm --network host -i -e PGPASSWORD=$DB_MASTER_PASSWORD postgres:15-alpine psql -h $DB_HOST -d $DB_DATABASE -U $DB_MASTER_USERNAME
+
+echo "Creating database schema"
+cat ./db-up.sql   | docker run --rm --network host -i -e PGPASSWORD=$DB_MASTER_PASSWORD postgres:15-alpine psql -h $DB_HOST -d $DB_DATABASE -U $DB_MASTER_USERNAME
+
+
+for index in ${!FILES[*]}; do
+  FILE=${FILES[$index]}
+  echo "Processing: $FILE"
+  
+  if [ ! -f "$FILE.csv" ]; then
+    echo "Decompressing $FILE.csv.gz"
+    if [[ ! -f $FILE.csv.gz ]]; then
+      echo "File $FILE.csv.gz does not exist."
+      exit 1
+    fi
+    gunzip -v -c $FILE.csv.gz > $FILE.csv
+  else
+    echo "File $FILE.csv already decompressed"
+  fi
+
+  echo "Processed: $FILE"
+  
+done
+
+for index in ${!FILES[*]}; do
+  FILE=${FILES[$index]}
+
+  echo "Importing $FILE"
+  
+  docker run --rm --network host -v /$(pwd)/:/data -e PGPASSWORD=$DB_MASTER_PASSWORD postgres:15-alpine psql -h $DB_HOST -d $DB_DATABASE -U $DB_MASTER_USERNAME \
+      -c "\copy $FILE FROM '/data/$FILE.csv' CSV HEADER;"
+
+  echo "Imported $FILE"
+done
